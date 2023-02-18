211bc692fdca13eaaac1f9712d960d35f306ce5e
Josh <joshua.lambert@knights.ucf.edu>
2023-02-18 11:05:35 -0600

    update app

--- app.py
+++ app.py
@@ -28,73 +28,87 @@ def get_data():
 
 
 def labeler():
-    df = get_data()
-    ids = df['ids'].to_list()
 
-    if "annotations" not in st.session_state:
-            st.session_state.annotations = {}
-            st.session_state.ids = ids
+    placeholder = st.empty()
+
+    with placeholder.form(key="login"):
+        user = st.text_input("Username")
+        password = st.text_input("Password")
+        st.form_submit_button("Login")
             
-    with st.sidebar:
-            st.sidebar.title('Projects')
+    if user == st.secrets["authentication"]['username'] and password == st.secrets["authentication"]['password']:
+            
+        df = get_data()
+        ids = df['ids'].to_list()
+
+        if "annotations" not in st.session_state:
+                st.session_state.annotations = {}
+                st.session_state.ids = ids
+                
+        with st.sidebar:
+                st.sidebar.title('Projects')
+                if st.session_state.ids:
+                    st.session_state.current_id = st.selectbox(
+                    'Select a unique id?',
+                    st.session_state.ids)
+
+
+        st.markdown("# Text Labeler")
+        st.markdown("--------")
+
+
+
+        left_column, _, right_column = st.columns([50, 2, 20])
+        with left_column:
+            
+
+
+            def annotate(label):
+                st.session_state.annotations[st.session_state.current_id] = label
+                if st.session_state.ids:
+                    #st.session_state.current_id = random.choice(st.session_state.ids)
+                    st.session_state.ids.remove(st.session_state.current_id)
+            st.markdown("Text to label:")
+            st.markdown("#### " + df.loc[df['ids'] == st.session_state.current_id]['text'].values[0])
+            st.markdown("---------")
+            st.markdown(" ")
+            st.markdown("labels:")
             if st.session_state.ids:
-                st.session_state.current_id = st.selectbox(
-                'Select a unique id?',
-                st.session_state.ids)
+                a, b = st.columns(2)
+                with a:
+                    st.button("This is a dog! 🐶", on_click=annotate, args=("dog",))
+                with b:
+                    st.button("This is a cat! 🐱", on_click=annotate, args=("cat",))
 
+            else:
+            
+                st.write("### Annotations")
+                st.write(st.session_state.annotations)
+                df = pd.DataFrame([st.session_state.annotations])
+                dfnew = df.T
+                dfnew.columns = ['labels']
+                dfnew = dfnew.reset_index()
+                sheet2 = client.open("output_data").sheet1
+                sheet2.update([dfnew.columns.values.tolist()] + dfnew.values.tolist())
 
-    st.markdown("# Text Labeler")
-    st.markdown("--------")
+        with right_column:
 
-
-
-    left_column, _, right_column = st.columns([50, 2, 20])
-    with left_column:
-        
-
-
-        def annotate(label):
-            st.session_state.annotations[st.session_state.current_id] = label
             if st.session_state.ids:
-                #st.session_state.current_id = random.choice(st.session_state.ids)
-                st.session_state.ids.remove(st.session_state.current_id)
-        st.markdown("Text to label:")
-        st.markdown("#### " + df.loc[df['ids'] == st.session_state.current_id]['text'].values[0])
-        st.markdown("---------")
-        st.markdown(" ")
-        st.markdown("labels:")
-        if st.session_state.ids:
-            a, b = st.columns(2)
-            with a:
-                st.button("This is a dog! 🐶", on_click=annotate, args=("dog",))
-            with b:
-                st.button("This is a cat! 🐱", on_click=annotate, args=("cat",))
+                st.write(
+                    "Annotated:",
+                    len(st.session_state.annotations),
+                    "– Remaining:",
+                    len(st.session_state.ids),
+                )
 
-        else:
-           
-            st.write("### Annotations")
-            st.write(st.session_state.annotations)
-            df = pd.DataFrame([st.session_state.annotations])
-            dfnew = df.T
-            dfnew.columns = ['labels']
-            dfnew = dfnew.reset_index()
-            sheet2 = client.open("output_data").sheet1
-            sheet2.update([dfnew.columns.values.tolist()] + dfnew.values.tolist())
+            else:
+                st.success(
+                    f"🎈 Done! All {len(st.session_state.annotations)} text annotated."
+                )
+        placeholder.empty()
 
-    with right_column:
-
-        if st.session_state.ids:
-            st.write(
-                "Annotated:",
-                len(st.session_state.annotations),
-                "– Remaining:",
-                len(st.session_state.ids),
-            )
-
-        else:
-            st.success(
-                f"🎈 Done! All {len(st.session_state.annotations)} text annotated."
-            )
+    else:
+        st.write("Incorrect username or password")
 
 if __name__ == "__main__":
     labeler()
